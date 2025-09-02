4722bc0746bc8f1b8d261aa596c9927d8035f8ad
wilia <wilia.francisco@souunit.com.br>
2025-09-02 08:30:11 -0300

    Interface

---
+++ components/unidadePrivativa/page.tsx
@@ -0,0 +1,17 @@
+export interface UnidadePrivativa {
+    id: number;
+    nome: string;
+}
+
+const UnidadePrivativaPage: UnidadePrivativa[] = [
+    { id: 1, nome: "Área Técnica" },
+    { id: 2, nome: "Circulação" },
+    { id: 3, nome: "Cozinha/Área de Serviço" },
+    { id: 4, nome: "Garden" },
+    { id: 4, nome: "Quarto e Suíte" },
+    { id: 6, nome: "Sanitário/Lavabo" },
+    { id: 7, nome: "Sala de Estar/Jantar" },
+    { id: 8, nome: "Varanda" },
+];
+
+export default UnidadePrivativaPage;
