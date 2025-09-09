ee7cd82d7aedb4d71770c325774991dd0a88fcbf
Samirborges <alessandro.samir@souunit.com.br>
2025-09-09 13:37:52 -0300

    Adicionando o DropDown

--- app/empreendimento/page.tsx
+++ app/empreendimento/page.tsx
@@ -1,9 +1,9 @@
-"use client"; 
+"use client";
 
 import React from "react";
 import Sidebar from "@/components/sections/Sidebar";
 import Button from "@/components/sections/Button";
-
+import DropDown from "@/components/sections/DropBox";
 
 const EmpreendimentoPage: React.FC = () => {
   return (
@@ -11,9 +11,8 @@ const EmpreendimentoPage: React.FC = () => {
       <div className="flex h-screen">
         <Sidebar />
         <div className="flex-1 bg-white p-6  shadow-md overflow-auto">
-          <h2 className="text-black font-semibold mb-4">
-            Adicionar Itens
-          </h2>
+          <h2 className="text-black font-semibold mb-4">Adicionar Itens</h2>
+          <DropDown options={["Opção 1", "Opção 2", "Opção 3"]} />
           <Button color="green" onClick={() => alert("Botão foi clicado")}>
             Adicionar Item
           </Button>
