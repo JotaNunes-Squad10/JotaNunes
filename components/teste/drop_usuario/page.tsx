d6dae1089a9e19419d4872a56fdd6e045624d9d5
Flávia Monique <flaviamonique38@gmail.com>
2025-09-15 15:40:27 -0300

    menu botão usuário

---
+++ components/teste/drop_usuario/page.tsx
@@ -0,0 +1,35 @@
+
+import React, { useRef } from 'react';
+import { Button } from 'primereact/button';
+import { TieredMenu } from 'primereact/tieredmenu';
+import { MenuItem } from 'primereact/menuitem';
+
+import { UserRound } from 'lucide-react'
+
+export default function PopupDemo() {
+    const menu = useRef(null);
+    const items: MenuItem[] = [
+        {
+            label: 'Sair',
+             url: '/'
+        },
+        {
+            label: 'Configuracoes',
+        },
+    ];
+
+    return (
+        <div className="card flex justify-content-center">
+            <TieredMenu model={items} popup ref={menu} breakpoint="700px" />
+            <Button label="Usuário" onClick={(e) => menu.current.toggle(e)} 
+              icon={<UserRound size={20} color="white" />} 
+              iconPos="left"
+                unstyled
+                    pt={{
+                        root: { className: 'bg-red-600 hover:bg-red-700 cursor-pointer text-white p-1 rounded flex gap-2' },
+                        label: { className: 'text-white font-bold text-lm' },
+                    }}/>
+        </div>
+    )
+}
+        
