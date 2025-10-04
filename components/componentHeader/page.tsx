090f392c1b489a028dbae907bc22a29290adab35
PedroLZambon <pedro.lovatto@souunit.com.br>
2025-10-04 19:26:41 -0300

    Alteração de responsividade e posição dos componentes

--- components/componentHeader/page.tsx
+++ components/componentHeader/page.tsx
@@ -17,25 +17,25 @@ export default function ActionBar() {
   };
 
   return (
-    <div className="flex flex-col sm:flex-row sm:justify-between gap-3 mb-6">
+    <div className="flex flex-row sm:justify-end gap-2 sm:gap-8 mb-6">
       {/* Botão à esquerda */}
       <Button
         label="Salvar"
-        className="p-button-success w-full sm:w-48"
+        className="p-button-success flex-1 min-w-[100px] max-w-[192px]"
         onClick={handleSalvar}
       />
 
       {/* Botão central */}
       <Button
         label="Status: Editando"
-        className="p-button-info w-full sm:w-48"
+        className="p-button-info flex-1 min-w-[100px] max-w-[192px]"
         onClick={handleStatus}
       />
 
       {/* Botão à direita */}
       <Button
         label="Exportar"
-        className="p-button-danger w-full sm:w-48"
+        className="p-button-danger flex-1 min-w-[100px] max-w-[192px]"
         onClick={handleExportar}
       />
     </div>
