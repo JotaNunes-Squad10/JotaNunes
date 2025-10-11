81583dc0a79035a21d91018d5e5692dd5d931640
Samirborges <alessandro.samir@souunit.com.br>
2025-10-11 12:50:05 -0300

    Removendo a notificação Toast

--- components/EmpreendimentoEditor/SideBar/PainelMenu/page.tsx
+++ components/EmpreendimentoEditor/SideBar/PainelMenu/page.tsx
@@ -1,11 +1,8 @@
-import React, { useRef } from "react";
 import { PanelMenu } from "primereact/panelmenu";
 import { MenuItem } from "primereact/menuitem";
-import { Toast } from "primereact/toast";
 
 export default function PainelMenu() {
   // Inicialização do Toast (mantida)
-  const toast = useRef<Toast | null>(null);
 
   const mockMenuItems: MenuItem[] = [
     {
@@ -18,27 +15,12 @@ export default function PainelMenu() {
           command: (event) => {
             // A função command deve ser adaptada para usar o mock
             // e o item específico.
-            if (toast.current) {
-              toast.current.show({
-                severity: "info",
-                summary: "Unidades privativas",
-                detail: `Item selecionado: Sala de Estar/Jantar`,
-                life: 3000,
-              });
-            }
           },
         },
         {
           label: "Circulação",
           icon: "pi pi-tag",
-          command: () => {
-            toast.current?.show({
-              severity: "info",
-              summary: "Unidades privativas",
-              detail: `Item selecionado: Circulação`,
-              life: 3000,
-            });
-          },
+          command: () => {},
         },
       ],
     },
@@ -49,26 +31,12 @@ export default function PainelMenu() {
         {
           label: "Guarita",
           icon: "pi pi-tag",
-          command: () => {
-            toast.current?.show({
-              severity: "info",
-              summary: "Área comum",
-              detail: `Item selecionado: Guarita`,
-              life: 3000,
-            });
-          },
+          command: () => {},
         },
         {
           label: "Gourmets",
           icon: "pi pi-tag",
-          command: () => {
-            toast.current?.show({
-              severity: "info",
-              summary: "Área comum",
-              detail: `Item selecionado: Gourmets`,
-              life: 3000,
-            });
-          },
+          command: () => {},
         },
       ],
     },
@@ -79,14 +47,7 @@ export default function PainelMenu() {
         {
           label: "Descrição Marcas",
           icon: "pi pi-tag",
-          command: () => {
-            toast.current?.show({
-              severity: "info",
-              summary: "Marcas",
-              detail: `Item selecionado: Descrição Marcas`,
-              life: 3000,
-            });
-          },
+          command: () => {},
         },
       ],
     },
@@ -99,7 +60,6 @@ export default function PainelMenu() {
     // O div 'card' foi mantido, mas o w-full foi removido do PanelMenu
     <div className="card flex justify-content-center">
       <PanelMenu model={items} className="w-full md:w-20rem" />
-      <Toast ref={toast} />
     </div>
   );
 }
