64ca61364b7a07b659daee8fdc044ff4eb4c64be
Samirborges <alessandro.samir@souunit.com.br>
2025-10-16 18:30:27 -0300

    Modificando o componente de Zambom

--- components/EmpreendimentoEditor/FormEditPage/page.tsx
+++ components/EmpreendimentoEditor/FormEditPage/page.tsx
@@ -32,48 +32,48 @@ export default function FormEmpreendimento({
   params,
 }: FormEmpreendimentoProps) {
   return (
-    <Card className="shadow-md p-6 w-full ">
+    <div>
       <ActionBar />
-      {/* Campo Empreendimento */}
-      <div className="flex flex-col sm:flex-row sm:items-center mb-6">
-        <label className="sm:w-40 font-semibold text-gray-700 mb-2 sm:mb-0">
-          Empreendimento:
-        </label>
-        <InputText
-          value={params.nomeDocumento}
-          onChange={(e) => params.setNomeDocumento(e.target.value)}
-          placeholder="Digite o nome do empreendimento"
-          className="flex-1"
-        />
-      </div>
-
-      {/* Campo Localização */}
-      <div className="flex flex-col sm:flex-row sm:items-center mb-6">
-        <label className="sm:w-40 font-semibold text-gray-700 mb-2 sm:mb-0">
-          Localização:
-        </label>
-        <InputText
-          value={params.localizacaoDocumento}
-          onChange={(e) => params.setLocalizacaoDocumento(e.target.value)}
-          placeholder="Digite a localização"
-          className="flex-1"
-        />
-      </div>
-
-      {/* Campo Descrição */}
-      <div>
-        <label className="block font-semibold mb-2 text-gray-700">
-          Descrição do Empreendimento:
-        </label>
-        <InputTextarea
-          value={params.descricaoDocumento}
-          onChange={(e) => params.setDescricaoDocumento(e.target.value)}
-          placeholder="Digite a descrição"
-          rows={6}
-          autoResize
-          className="w-full"
-        />
-      </div>
-    </Card>
+      <Card className="shadow-md p-6 w-full ">
+        {/* Campo Empreendimento */}
+        <div className="flex flex-col sm:flex-row sm:items-center mb-6">
+          <label className="sm:w-40 font-semibold text-gray-700 mb-2 sm:mb-0">
+            Empreendimento:
+          </label>
+          <InputText
+            value={params.nomeDocumento}
+            onChange={(e) => params.setNomeDocumento(e.target.value)}
+            placeholder="Digite o nome do empreendimento"
+            className="flex-1"
+          />
+        </div>
+        {/* Campo Localização */}
+        <div className="flex flex-col sm:flex-row sm:items-center mb-6">
+          <label className="sm:w-40 font-semibold text-gray-700 mb-2 sm:mb-0">
+            Localização:
+          </label>
+          <InputText
+            value={params.localizacaoDocumento}
+            onChange={(e) => params.setLocalizacaoDocumento(e.target.value)}
+            placeholder="Digite a localização"
+            className="flex-1"
+          />
+        </div>
+        {/* Campo Descrição */}
+        <div>
+          <label className="block font-semibold mb-2 text-gray-700">
+            Descrição do Empreendimento:
+          </label>
+          <InputTextarea
+            value={params.descricaoDocumento}
+            onChange={(e) => params.setDescricaoDocumento(e.target.value)}
+            placeholder="Digite a descrição"
+            rows={6}
+            autoResize
+            className="w-full"
+          />
+        </div>
+      </Card>
+    </div>
   );
 }
