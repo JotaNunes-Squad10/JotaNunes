d9487fafcedeb38188ecbb2d44c77ae8ab6c9397
Samirborges <alessandro.samir@souunit.com.br>
2025-10-30 21:20:04 -0300

    Add breakpoints to Dialog component in AdicionarNovoItem

--- components/EmpreendimentoEditor/AddedItemsInDocument/AdicionarItensDocumento/AdicionarNovoItem/page.tsx
+++ components/EmpreendimentoEditor/AddedItemsInDocument/AdicionarItensDocumento/AdicionarNovoItem/page.tsx
@@ -40,6 +40,7 @@ export default function AdicionarNovoItem({ onReload }: Props) {
         visible={visible}
         modal={false}
         style={{ width: "50vw" }}
+        breakpoints={{ "960px": "75vw", "640px": "90vw" }}
         onHide={() => {
           if (!visible) return;
           setVisible(false);
