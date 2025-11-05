d2328d0a801b99af77fbdb61be6eacbde43e83d3
Flávia Monique <flaviamonique38@gmail.com>
2025-11-04 22:32:42 -0300

    menu de açoes rápidas com acesso so pra altorizados

--- components/home/menu_acoes_rapidas/page.tsx
+++ components/home/menu_acoes_rapidas/page.tsx
@@ -1,29 +1,66 @@
+"use client";
+
 import React, { useRef } from 'react';
 import { Button } from 'primereact/button';
 import { TieredMenu } from 'primereact/tieredmenu';
 import { MenuItem } from 'primereact/menuitem';
-
+import { jwtDecode } from "jwt-decode";
+import Cookies from "js-cookie";
 
 import { Plus  } from 'lucide-react'
 
+interface JwtPayload {
+  groups?: string[];
+}
+
 export default function Menu_acoes_rapidas() {
     const menu = useRef(null);
-    const items: MenuItem[] = [
-        {
-            label: 'Criar Novo Documento',
-             url: ''
-        },
-        {
-            label: 'Criar a partir do Padrão Torre PC - Mais Viver',
-        },
-          {
-            label: 'Criar a partir do Padrão Torre PC - Residence',
-        },
-          {
-            label: 'Criar a partir do Padrão Sobrado PC - Mais Viver',
-        },
-    ];
 
+    const token = Cookies.get("accessToken");
+    let isAdmin = false;
+    let isOperador = false;
+
+    if (token) {
+      try {   
+        const payload = jwtDecode<JwtPayload>(token);
+
+        if (payload.groups?.includes("Administrador")) {
+          isAdmin = true;
+        } 
+        if (payload.groups?.includes("Operador")) {
+            isOperador = true;
+        }
+
+      } catch (err) {
+        console.error("Erro ao decodificar JWT:", err);
+      }
+    }
+
+    const items: MenuItem[] = [
+
+        ...(isAdmin || isOperador 
+        ? [
+             {
+                label: 'Criar Novo Documento',
+                url: ''
+            },
+
+            {
+                label: 'Criar a partir do Padrão Torre PC - Mais Viver',
+            },
+
+            {
+                label: 'Criar a partir do Padrão Torre PC - Residence',
+            },
+
+            {
+                label: 'Criar a partir do Padrão Sobrado PC - Mais Viver',
+            },
+        ]
+         : []),
+        ]
+
+    
     return (
         <div className="card flex justify-content-center">
             <TieredMenu model={items} popup ref={menu} breakpoint="700px" />
