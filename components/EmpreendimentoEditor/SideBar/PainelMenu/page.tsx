57bf5562daf8dd8140e672e6c6d24c780683323b
Samirborges <alessandro.samir@souunit.com.br>
2025-11-05 17:57:23 -0300

    Refactor child item creation in PainelMenu component; improve environment addition and deletion logic

--- components/EmpreendimentoEditor/SideBar/PainelMenu/page.tsx
+++ components/EmpreendimentoEditor/SideBar/PainelMenu/page.tsx
@@ -79,7 +79,8 @@ export default function PainelMenu({
       let childItems: MenuItem[] = [];
 
       subTopicos.forEach((sub) => {
-        if (sub.topico.id === topico.id && sub.nome.trim() !== "") {
+        // Tópicos que possui ambientes
+        if (sub.topico.id === topico.id) {
           childItems.push({
             label: sub.nome,
             icon: "",
@@ -113,56 +114,60 @@ export default function PainelMenu({
           // Caso não haja sub-tópicos, adiciona um item padrão
           const idTopic = topico.id;
           if (childItems.length === 0 && ![1, 2, 3].includes(idTopic)) {
-            childItems.push(
-              {
-                label: "Adicione um novo ambiente",
-                template: (item, options) => {
-                  return (
-                    <div
-                      onClick={options.onClick}
-                      className={`
+            childItems.push({
+              label: "Adicione um novo ambiente",
+              template: (item, options) => {
+                return (
+                  <div
+                    onClick={options.onClick}
+                    className={`
                       flex items-center
                       px-3 py-2
                       rounded-md
                       cursor-pointer
                       color-gray-500
                     `}
-                    >
-                      Adicione um novo item
-                    </div>
-                  );
-                },
+                  >
+                    Adicione um novo item
+                  </div>
+                );
               },
-              {
-                label: "Excluir Ambiente",
-                icon: "",
-                command: async () => {
-                  try {
-                    const idTopicoPayload: DeleteTopicPayload = {
-                      id: topico.id,
-                    };
-                    await topicoService.deleteTopic(idTopicoPayload);
+            });
+          }
+        }
+      });
 
-                    // Realizando regarga dos tópicos
-                    const novosTopicos = await topicoService.getAllTopic();
-                    const novosSubTopicos =
-                      await subTopicosAmbienteService.getAllAmbiente();
+      if (![1, 2, 3].includes(topico.id)) {
+        childItems.push({
+          label: "Excluir Ambiente",
+          icon: "",
+          command: async () => {
+            try {
+              const idTopicoPayload: DeleteTopicPayload = {
+                id: topico.id,
+              };
+              await topicoService.deleteTopic(idTopicoPayload);
 
-                    setTopicos(novosTopicos);
-                    setSubTopicos(novosSubTopicos);
-                  } catch (error) {
-                    console.error("Erro ao excluir o tópico:", error);
-                  }
-                },
-                template: (item, options) => {
-                  const isSelected =
-                    ambienteSelecionado === topico.nome &&
-                    itemAmbienteSelecionado === "Excluir Ambiente";
+              // Realizando regarga dos tópicos
+              const novosTopicos = await topicoService.getAllTopic();
+              const novosSubTopicos =
+                await subTopicosAmbienteService.getAllAmbiente();
 
-                  return (
-                    <div
-                      onClick={options.onClick}
-                      className={`
+              setTopicos(novosTopicos);
+              setSubTopicos(novosSubTopicos);
+            } catch (error) {
+              console.error("Erro ao excluir o tópico:", error);
+            }
+          },
+          template: (item, options) => {
+            const isSelected =
+              ambienteSelecionado === topico.nome &&
+              itemAmbienteSelecionado === "Excluir Ambiente";
+
+            return (
+              <div
+                onClick={options.onClick}
+                className={`
                       flex items-center
                       px-3 py-2
                       rounded-md
@@ -176,232 +181,14 @@ export default function PainelMenu({
                           : "bg-red-100 text-red-700"
                       }
                     `}
-                    >
-                      <i className="pi pi-times mr-3 text-lg" />
-                      Excluir Ambiente
-                    </div>
-                  );
-                },
-              }
+              >
+                <i className="pi pi-times mr-3 text-lg" />
+                Excluir Ambiente
+              </div>
             );
-          }
-        }
-      });
-
-      // if (topico.nome === "ÁREA COMUM") {
-      //   subTopicos.forEach((sub) => {
-      //     if (sub.topico.id === topico.id) {
-      //       childItems.push({
-      //         label: sub.nome,
-      //         icon: "",
-      //         command: () => {
-      //           setAmbienteSelecionado(topico.nome);
-      //           setItemAmbienteSelecionado(sub.nome);
-      //         },
-      //         template: (item, options) => {
-      //           const isSelected =
-      //             ambienteSelecionado === topico.nome &&
-      //             itemAmbienteSelecionado === sub.nome;
-
-      //           return (
-      //             <div
-      //               onClick={options.onClick}
-      //               style={{
-      //                 backgroundColor: isSelected ? "#dc2626" : "transparent", // vermelho
-      //                 color: isSelected ? "#ffffff" : "inherit",
-      //                 fontWeight: isSelected ? "bold" : "normal",
-      //                 padding: "0.5rem 0.75rem",
-      //                 borderRadius: "0.375rem",
-      //                 cursor: "pointer",
-      //               }}
-      //             >
-      //               {sub.nome}
-      //             </div>
-      //           );
-      //         },
-      //       });
-      //     }
-      //   });
-      // } else if (topico.nome === "UNIDADES PRIVATIVAS") {
-      //   subTopicos.forEach((sub) => {
-      //     if (sub.topico.id === topico.id) {
-      //       childItems.push({
-      //         label: sub.nome,
-      //         icon: "",
-      //         command: () => {
-      //           setAmbienteSelecionado(topico.nome);
-      //           setItemAmbienteSelecionado(sub.nome);
-      //         },
-      //         template: (item, options) => {
-      //           const isSelected =
-      //             ambienteSelecionado === topico.nome &&
-      //             itemAmbienteSelecionado === sub.nome;
-
-      //           return (
-      //             <div
-      //               onClick={options.onClick}
-      //               style={{
-      //                 backgroundColor: isSelected ? "#dc2626" : "transparent", // vermelho
-      //                 color: isSelected ? "#ffffff" : "inherit",
-      //                 fontWeight: isSelected ? "bold" : "normal",
-      //                 padding: "0.5rem 0.75rem",
-      //                 borderRadius: "0.375rem",
-      //                 cursor: "pointer",
-      //               }}
-      //             >
-      //               {sub.nome}
-      //             </div>
-      //           );
-      //         },
-      //       });
-      //     }
-      //   });
-      // } else if (topico.nome === "MARCAS") {
-      //   childItems = [
-      //     {
-      //       label: "Descrição Marcas",
-      //       icon: "",
-      //       command: () => {
-      //         setAmbienteSelecionado(topico.nome);
-      //         setItemAmbienteSelecionado("Descrição Marcas");
-      //       },
-      //       template: (item, options) => {
-      //         const isSelected =
-      //           ambienteSelecionado === topico.nome &&
-      //           itemAmbienteSelecionado === "Descrição Marcas";
-
-      //         return (
-      //           <div
-      //             onClick={options.onClick}
-      //             style={{
-      //               backgroundColor: isSelected ? "#dc2626" : "transparent",
-      //               color: isSelected ? "#ffffff" : "inherit",
-      //               fontWeight: isSelected ? "bold" : "normal",
-      //               padding: "0.5rem 0.75rem",
-      //               borderRadius: "0.375rem",
-      //               cursor: "pointer",
-      //             }}
-      //           >
-      //             Descrição Marcas
-      //           </div>
-      //         );
-      //       },
-      //     },
-      //   ];
-      // }
-      // else {
-      //   childItems = [
-      //     {
-      //       label: "Item 1",
-      //       icon: "",
-      //       command: () => {
-      //         setAmbienteSelecionado(topico.nome);
-      //         setItemAmbienteSelecionado("Descrição Marcas");
-      //       },
-      //       template: (item, options) => {
-      //         const isSelected =
-      //           ambienteSelecionado === topico.nome &&
-      //           itemAmbienteSelecionado === "Descrição Marcas";
-
-      //         return (
-      //           <div
-      //             onClick={options.onClick}
-      //             style={{
-      //               backgroundColor: isSelected ? "#dc2626" : "transparent",
-      //               color: isSelected ? "#ffffff" : "inherit",
-      //               fontWeight: isSelected ? "bold" : "normal",
-      //               padding: "0.5rem 0.75rem",
-      //               borderRadius: "0.375rem",
-      //               cursor: "pointer",
-      //             }}
-      //           >
-      //             Item 1
-      //           </div>
-      //         );
-      //       },
-      //     },
-      //     {
-      //       label: "Item 2",
-      //       icon: "",
-      //       command: () => {
-      //         setAmbienteSelecionado(topico.nome);
-      //         setItemAmbienteSelecionado("Descrição Marcas");
-      //       },
-      //       template: (item, options) => {
-      //         const isSelected =
-      //           ambienteSelecionado === topico.nome &&
-      //           itemAmbienteSelecionado === "Descrição Marcas";
-
-      //         return (
-      //           <div
-      //             onClick={options.onClick}
-      //             style={{
-      //               backgroundColor: isSelected ? "#dc2626" : "transparent",
-      //               color: isSelected ? "#ffffff" : "inherit",
-      //               fontWeight: isSelected ? "bold" : "normal",
-      //               padding: "0.5rem 0.75rem",
-      //               borderRadius: "0.375rem",
-      //               cursor: "pointer",
-      //             }}
-      //           >
-      //             Item 2
-      //           </div>
-      //         );
-      //       },
-      //     },
-      //     // Deve ser sempre o ultimo item!
-      //     {
-      //       label: "Excluir Ambiente",
-      //       icon: "",
-      //       command: async () => {
-      //         try {
-      //           const idTopicoPayload: DeleteTopicPayload = {
-      //             id: topico.id,
-      //           };
-      //           await topicoService.deleteTopic(idTopicoPayload);
-
-      //           // Realizando regarga dos tópicos
-      //           const novosTopicos = await topicoService.getAllTopic();
-      //           const novosSubTopicos =
-      //             await subTopicosAmbienteService.getAllAmbiente();
-
-      //           setTopicos(novosTopicos);
-      //           setSubTopicos(novosSubTopicos);
-      //         } catch (error) {
-      //           console.error("Erro ao excluir o tópico:", error);
-      //         }
-      //       },
-      //       template: (item, options) => {
-      //         const isSelected =
-      //           ambienteSelecionado === topico.nome &&
-      //           itemAmbienteSelecionado === "Descrição Marcas";
-
-      //         return (
-      //           <div
-      //             onClick={options.onClick}
-      //             className={`
-      //               flex items-center
-      //               px-3 py-2
-      //               rounded-md
-      //               cursor-pointer
-      //               text-red-700
-      //               hover:bg-red-600
-      //               hover:text-white
-      //               ${
-      //                 isSelected
-      //                   ? "bg-red-600 text-white font-bold"
-      //                   : "bg-red-100 text-red-700"
-      //               }
-      //             `}
-      //           >
-      //             <i className="pi pi-times mr-3 text-lg" />
-      //             Excluir Ambiente
-      //           </div>
-      //         );
-      //       },
-      //     },
-      //   ];
-      // }
+          },
+        });
+      }
 
       return {
         label: `${index + 1}. ${topico.nome}`,
