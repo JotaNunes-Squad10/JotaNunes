df402b84a9d9c8952d7453106509581a6dee9587
mariounit <149197396+mariounit@users.noreply.github.com>
2025-11-23 16:54:15 -0300

    toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -29,6 +31,7 @@ export default function RootLayout({
     <html lang="en" className={`${inter.variable} ${robotoMono.variable}`}>
       <body className="antialiased">
         {children}
+        <ToastContainer autoClose={2000} theme="colored" />
       </body>
     </html>
   );
