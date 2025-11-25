21512092732f3c5c2c1335ec92c2f74c03091322
mariounit <149197396+mariounit@users.noreply.github.com>
2025-11-25 09:39:30 -0300

    Remove toast de layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import "primeicons/primeicons.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -32,7 +30,6 @@ export default function RootLayout({
     <html lang="en" className={`${inter.variable} ${robotoMono.variable}`}>
       <body className="antialiased">
         {children}
-        <ToastContainer autoClose={2000} theme="colored" />
       </body>
     </html>
   );
